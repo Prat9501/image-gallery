1be940fa0535a5a7cda3e4b5c8fc97405a64300e
Prateek Gupta <prateekg.9501@gmail.com>
2020-05-30 15:59:49 +0530

    added unsplash api integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+
 export default function App() {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://api.unsplash.com/photos?client_id=${accessKey}`).then(
+        res => res.json()
+      ).then(setImages);
+  }, []);
+
+  if (!accessKey){
+    return (
+      <a href="https://unsplash.com/developers" className='error'>
+        Get your Unsplash API Key first.
+      </a>
+    )
+  };
+
   return (
     <div className="app">
       <h1>Unsplash Image Gallery!</h1>
@@ -12,9 +30,9 @@ export default function App() {
       </form>
 
       <div className="image-grid">
-        {[...Array(10)].map((_, index) => (
+        {images.map((image, index) => (
           <div className="image" key={index}>
-            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
+            <img src={image.urls.regular} alt={image.alt_description} />
           </div>
         ))}
       </div>
