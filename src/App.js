75bb48037c0e9ba8c87918fe9704e990ec0f9988
Prateek Gupta <prateekg.9501@gmail.com>
2020-05-31 00:35:30 +0530

    handled console error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import './App.css';
 
@@ -7,52 +7,75 @@ const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 export default function App() {
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+
+  const getPhotos = useCallback(() => {
+    let apiUrl = `https://api.unsplash.com/photos?`;
+    if (query) apiUrl = `https://api.unsplash.com/search/photos?query=${query}`;
+    apiUrl += `&page=${page}`;
+    apiUrl += `&client_id=${accessKey}`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        const imagesFromApi = data.results ?? data;
+        if (page === 1) {
+          setImages(imagesFromApi);
+          return;
+        }
+        setImages((images) => [...images, ...imagesFromApi]);
+      });
+  }, [page, query]);
+
   useEffect(() => {
     getPhotos();
-  }, [page]);
+  }, [page, getPhotos]);
 
-  function getPhotos(){
-    fetch(
-      `https://api.unsplash.com/photos?client_id=${accessKey}&page=${page}`).then(
-        res => res.json()
-      ).then(data => {
-        setImages(images => [...images, ...data])
-      });
+  function searchPhotos(e) {
+    e.preventDefault();
+    setPage(1);
+    getPhotos();
   }
-
-  if (!accessKey){
+  if (!accessKey) {
     return (
-      <a href="https://unsplash.com/developers" className='error'>
-        Get your Unsplash API Key first.
+      <a href="https://unsplash.com/developers" className="error">
+        Required: Get Your Unsplash API Key First
       </a>
-    )
-  };
+    );
+  }
 
   return (
     <div className="app">
       <h1>Unsplash Image Gallery!</h1>
 
-      <form>
-        <input type="text" placeholder="Search Unsplash..." />
+      <form onSubmit={searchPhotos}>
+        <input
+          type="text"
+          placeholder="Search Unsplash..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
         <button>Search</button>
       </form>
 
-      <InfiniteScroll 
-      dataLength={images.length}
-      next={() => {
-        setPage(page => page + 1);
-      }}
-      hasMore={true}
-      loader={<h4>Loading...</h4>}>
-
-      <div className="image-grid">
-        {images.map((image, index) => (
-          <a className="image" key={index} href={image.links.html}
-            target="_blank" rel="noopener noreferrer">
-            <img src={image.urls.regular} alt={image.alt_description} />
-          </a>
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={images.length}
+        next={() => setPage((page) => page + 1)}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="image-grid">
+          {images.map((image, index) => (
+            <a
+              className="image"
+              key={index}
+              href={image.links.html}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={image.urls.regular} alt={image.alt_description} />
+            </a>
+          ))}
+        </div>
       </InfiniteScroll>
     </div>
   );
