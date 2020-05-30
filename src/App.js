17dcce3d1e1a3a192a9381f62b07d13cf355c6b8
Prateek Gupta <prateekg.9501@gmail.com>
2020-05-30 23:26:50 +0530

    added infinite scroll module

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './App.css';
 
 const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
 export default function App() {
   const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
   useEffect(() => {
+    getPhotos();
+  }, [page]);
+
+  function getPhotos(){
     fetch(
-      `https://api.unsplash.com/photos?client_id=${accessKey}`).then(
+      `https://api.unsplash.com/photos?client_id=${accessKey}&page=${page}`).then(
         res => res.json()
-      ).then(setImages);
-  }, []);
+      ).then(data => {
+        setImages(images => [...images, ...data])
+      });
+  }
 
   if (!accessKey){
     return (
@@ -29,13 +37,23 @@ export default function App() {
         <button>Search</button>
       </form>
 
+      <InfiniteScroll 
+      dataLength={images.length}
+      next={() => {
+        setPage(page => page + 1);
+      }}
+      hasMore={true}
+      loader={<h4>Loading...</h4>}>
+
       <div className="image-grid">
         {images.map((image, index) => (
-          <div className="image" key={index}>
+          <a className="image" key={index} href={image.links.html}
+            target="_blank" rel="noopener noreferrer">
             <img src={image.urls.regular} alt={image.alt_description} />
-          </div>
+          </a>
         ))}
       </div>
+      </InfiniteScroll>
     </div>
   );
 }
